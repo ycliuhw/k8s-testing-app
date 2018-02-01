a874f851e0c17e61c935f74487835b122e9c08c0
Kelvin Liu <kelvin.liu@domain.com.au>
2018-02-01 11:47:31 +1100

    Kelvin: added more routes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = () => 'Welcome to Kubernates :> !!!'
+const { del, get, post, router } = require("microrouter");
+
+const micro = require("micro");
+
+const handler = (req, res) => {
+    console.log(Object.keys(req))
+    // console.log(req);
+    return `Welcome to Kubernates :> !!!  path -> ${req.url}`;
+}
+
+const handlerPong = (req, res) => {
+    return 'pong'
+}
+
+const routers = router(
+    get("/fe-search/ping", handlerPong),
+    get("/*", handler),
+    post("/*", handler),
+    del("/*", handler)
+);
+
+const server = micro(routers);
+server.listen(3000);
